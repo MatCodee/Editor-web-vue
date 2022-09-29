3d612ee48a6711a30807a9451f0d7a919f6d0f79
Matias Altamirano <matytiyu4@gmail.com>
2022-09-29 03:59:09 -0300

    d

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,42 @@
   <div class="home">
     <div class="container-editor">
       <h1 class="py-4">Code Editor</h1>
-      <select
-          v-model="languageId"
-          class="form-select"
-          style="width: 50%"
-          aria-label="Default select example"
-          @change="changeLanguage()"
-        >
-        <option value="70" selected>Python</option>
-        <option value="76">C++</option>
-        <option value="62">Java</option>
-        </select>
+      <div class="d-flex container-selected">
+        <div>
+          <p>Select Languaje</p>
+          <select
+              v-model="languageId"
+              class="form-select"
+              style="width: 100%"
+              aria-label="Default select example"
+              @change="changeLanguage()"
+            >
+            <option value="70" selected>Python</option>
+            <option value="76">C++</option>
+            <option value="62">Java</option>
+          </select>
+        </div>
+        <div>
+          <p>Select font size</p>
+          <select
+              v-model="fontId"
+              class="form-select"
+              style="width: 100%"
+              aria-label="Default select example"
+              @change="change_font()"
+            >
+            
+            <option v-for="i,index in 40" :value="i" :key="index" selected>{{i}}</option>
+          </select>
+        </div>
+      </div>
+      <button class="btn btn-primary mt-3 pt-2" @click="run_program">
+        Run Program
+      </button>
 
       <brace
-        style="height: 500px; margin-top: 20px"
-        :fontsize="'12px'"
+        style="height: 73vh; margin-top: 20px"
+        :fontsize="this.current_font + 'px'"
         :theme="'tomorrow'"
         :mode="this.currentLanguage"
         :codefolding="'markbegin'"
@@ -24,9 +45,6 @@
         :selectionstyle="'text'"
         :highlightline="true" >
       </brace>
-      <button class="btn btn-primary pt-2">
-        Run Program
-      </button>
     </div>
   </div>
 </template>
@@ -38,8 +56,10 @@ export default {
   name: "Home",
   data() {
     return {
+      current_font: 12,
       currentLanguage: "python",
       languageId: 70,
+      fontId: 12,
       languageMap: {
         70: "python",
         76: "cpp",
@@ -56,20 +76,33 @@ export default {
       this.line_code = this.code.split("\n");
       console.log(this.line_code);
     },
+    change_font() {
+      this.current_font = this.fontId;
+    },
     changeLanguage() {
       console.log("languageId", this.languageId);
       this.requestBody.language_id = this.languageId.toString();
       this.currentLanguage = this.languageMap[this.languageId];
     },
+    // correr el programa
+    run_program() {
+      
+    }
   },
 };
 </script>
 <style lang="scss" scoped>
 .container-editor {
   width: 100%;
-  max-width: 1000px;
-  margin: 0 auto;
+  height: 100%;
+  padding: 0 20px;
+}
+.container-selected {
+  width: 100%;
 }
 
+.container-selected div {
+  width: 50%;
+}
 
 </style>
